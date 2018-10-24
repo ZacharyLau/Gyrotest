58dcea2103d4ff48fc3675ccf6d5afa05c5e1ef9
zach_liu <yitachi@gmail.com>
2018-10-24 16:39:11 -0400

    upload

--- Gyrotest/ViewController.swift
+++ Gyrotest/ViewController.swift
@@ -6,20 +6,89 @@
 //  Copyright © 2018 Zachary Kanzhe Liu. All rights reserved.
 //
 
+//unit tool
+
+//gyro
+
+
+
 import UIKit
+//gyroscope package
+import CoreMotion
+
 
 class ViewController: UIViewController {
 
+    //init motion manager
+    //var motion = CMMotionManager()
+    let m = gfunction()
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+        m.staticUpper = 0.033
+        m.StaticLower = -0.033
+        m.goodResetLower = -0.30
+        m.goodActionUpper = 0.30
+        m.formConstrainPitch = 65.0
+        m.quantity = 5
+        
+//        m.labelx = labelx
+//        m.labely = labely
+//        m.labelz = labelz
+//
+        m.setUpdateInterval(time: 0.02)
+        m.startRecord()
+       
+        print("set finished")
+
+        
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBOutlet weak var labelx: UILabel!
+    @IBOutlet weak var labely: UILabel!
+    @IBOutlet weak var labelz: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
 
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+      
+            if sender==button1{
+                labelx.text = "0"
+                labely.text = "0"
+                labelz.text = "0"
+            }
+
+    }
+
+    @IBAction func button2Action(_ sender: UIButton) {
+        
+        if sender==button2{
+            labelx.text = "0"
+            labely.text = "0"
+            labelz.text = "0"
+            let l = functions()
+            print("number from function: " , l.number(string: "yolo"))
+        }
+
+    }
 }
 
---
+++ Gyrotest/functions.swift
@@ -0,0 +1,19 @@
+//
+//  functions.swift
+//  Gyrotest
+//
+//  Created by Zachary Kanzhe Liu on 2018-05-14.
+//  Copyright © 2018 Zachary Kanzhe Liu. All rights reserved.
+//
+
+import Foundation
+
+class functions {
+    func number(string: String)->Int {
+        print(string)
+        return 23
+        
+    }
+    
+    
+}
